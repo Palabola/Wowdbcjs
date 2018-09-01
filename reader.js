82d1818c36cad71f71eb536d541fec074e35837d
Palabola <Palabola@users.noreply.github.com>
2018-09-01 16:29:22 +0200

    Experimently work
    Experimently work

--- reader.js
+++ reader.js
@@ -242,7 +242,7 @@ class Reader {
             }
             this.recordFormat[fieldId]['signed'] = false;
             if (fieldId > 0) {
-                this.recordFormat[$fieldId - 1]['valueCount'] =
+                this.recordFormat[fieldId - 1]['valueCount'] =
                 Math.floor((this.recordFormat[fieldId]['offset'] - this.recordFormat[fieldId - 1]['offset']) / this.recordFormat[fieldId - 1]['valueLength']);
             }
         }
@@ -252,11 +252,256 @@ class Reader {
         console.log(this.recordFormat);
 
 
+        let fieldId = this.fieldCount - 1;
+        let remainingBytes = this.recordSize - this.recordFormat[fieldId]['offset'];
+        this.recordFormat[fieldId]['valueCount'] = Math.max(1, Math.floor(remainingBytes / this.recordFormat[fieldId]['valueLength']));
+        if (this.recordFormat[fieldId]['valueCount'] > 1 &&    // we're guessing the last field is an array
+            ((this.recordSize % 4 == 0 && remainingBytes <= 4) // records may be padded to word length and the last field size <= word size
+             || (!this.hasIdBlock && this.idField == fieldId)// or the reported ID field is the last field
+             || this.hasEmbeddedStrings)) {                     // or we have embedded strings
+            this.recordFormat[fieldId]['valueCount'] = 1;      // assume the last field is scalar, and the remaining bytes are just padding
+        }
+
+        let commonBlockPointer = 0;
+        let palletBlockPointer = 0;
+
+        // Re-check file pointer!
+        if(this.file_pointer != this.fieldStorageInfoPos){
+                this.file_pointer  = this.fieldStorageInfoPos;                  
+        }
+
+
+
+       // $storageInfoFormat = 'voffsetBits/vsizeBits/VadditionalDataSize/VstorageType/VbitpackOffsetBits/VbitpackSizeBits/VarrayCount';
+
+        for (let fieldId = 0; fieldId < this.fieldCount; fieldId++) {
+            //parts = unpack($storageInfoFormat, fread($this->fileHandle, 24));
+
+            let parts = {}
+
+            parts['offsetBits'] = this.get_int_value(this.file_pointer,'uint16');
+            parts['sizeBits'] = this.get_int_value(this.file_pointer,'uint16');
+            parts['additionalDataSize'] = this.get_int_value(this.file_pointer,'uint32');
+            parts['storageType'] = this.get_int_value(this.file_pointer,'uint32');
+            parts['bitpackOffsetBits'] = this.get_int_value(this.file_pointer,'uint32');
+            parts['bitpackSizeBits'] = this.get_int_value(this.file_pointer,'uint32');
+            parts['arrayCount'] = this.get_int_value(this.file_pointer,'uint32');
+  
+
+            switch (parts['storageType']) {
+                case FIELD_COMPRESSION_COMMON:
+                  /*  this.recordFormat[fieldId]['size'] = 4;
+                    this.recordFormat[fieldId]['type'] = FIELD_TYPE_INT;
+                    this.recordFormat[fieldId]['valueCount'] = 1;
+                    parts['defaultValue'] = pack('V', parts['bitpackOffsetBits']);
+                    parts['bitpackOffsetBits'] = 0;
+                    parts['blockOffset'] = $commonBlockPointer;
+                    $commonBlockPointer += parts['additionalDataSize'];*/
+                    break;
+                case FIELD_COMPRESSION_BITPACKED_SIGNED:
+                    this.recordFormat[fieldId]['signed'] = true;
+                    // fall through
+                case FIELD_COMPRESSION_BITPACKED:
+                    this.recordFormat[fieldId]['size'] = 4;
+                    this.recordFormat[fieldId]['type'] = FIELD_TYPE_INT;
+                    this.recordFormat[fieldId]['offset'] = Math.floor(parts['offsetBits'] / 8);
+                    this.recordFormat[fieldId]['valueLength'] =  Math.ceil((parts['offsetBits'] + parts['sizeBits']) / 8) - this.recordFormat[fieldId]['offset'] + 1;
+                    this.recordFormat[fieldId]['valueCount'] = 1;
+                    break;
+                case FIELD_COMPRESSION_BITPACKED_INDEXED:
+                case FIELD_COMPRESSION_BITPACKED_INDEXED_ARRAY:
+                 /*   this.recordFormat[fieldId]['size'] = static::guessPalletFieldSize($palletBlockPointer, parts['additionalDataSize']);
+                    this.recordFormat[fieldId]['type'] =
+                        this.recordFormat[fieldId]['size'] == 4 ?
+                            static::guessPalletFieldType($palletBlockPointer, parts['additionalDataSize']) :
+                            FIELD_TYPE_INT;
+                    this.recordFormat[fieldId]['offset'] = floor(parts['offsetBits'] / 8);
+                    this.recordFormat[fieldId]['valueLength'] = ceil((parts['offsetBits'] + parts['sizeBits']) / 8) - this.recordFormat[fieldId]['offset'] + 1;
+                    this.recordFormat[fieldId]['valueCount'] = parts['arrayCount'] > 0 ? parts['arrayCount'] : 1;
+                    parts['blockOffset'] = $palletBlockPointer;
+                    $palletBlockPointer += parts['additionalDataSize'];*/
+                    break;
+                case FIELD_COMPRESSION_NONE:
+                    if (parts['arrayCount'] > 0) {
+                        this.recordFormat[fieldId]['valueCount'] = parts['arrayCount'];
+                    }
+                    break;
+                default:
+                    throw "Unknown field compression type ID: %d";
+            }
+
+            this.recordFormat[fieldId]['storage'] = parts;
+        }
+        //End loop
+
+        console.log('Record format:',this.recordFormat[0]['storage']);
+
+        console.log(this.fileSize);
+        console.log(this.file_pointer);
+
+
+
+        console.log("Record buffer: ",this.file_buffer.slice(this.file_pointer,this.fileSize));
+
+
+        // Get record values
+        for(let i = 0; i < this.recordCount;i++)
+        {
+
+           let value =  this.get_record_value(this.file_pointer,this.recordSize,this.recordFormat[0]['storage']['storageType']);
+
+           console.log('ID: '+(i+1)+" value: "+value);
+
+
+        }
 
 
 
 
 
+
+
+
+        if (!this.hasIdBlock) {
+            if (this.idField >= this.fieldCount) {
+                "Expected ID field " + this.idField +  " does not exist. Only found " + this.fieldCount + " fields.";
+            }
+            if (this.recordFormat[this.idField]['valueCount'] != 1) {
+                throw "Expected ID field " + this.idField + " reportedly has " + this.recordFormat[this.idField]['valueCount'] + " values per row";
+            }
+        }
+
+        if (hasRelationshipData) {
+            this.recordFormat[this.totalFieldCount++] = {
+                'valueLength' : 4,
+                'size' : 4,
+                'offset' : this.recordSize,
+                'type' : FIELD_TYPE_INT,
+                'valueCount' : 1,
+                'signed' : false,
+                'storage' : {
+                'storageType' : FIELD_COMPRESSION_NONE
+            }
+            };
+        }
+
+
+        if (this.hasEmbeddedStrings) {
+            for (let fieldId = 0; fieldId < this.fieldCount; fieldId++) {
+                if (this.recordFormat[fieldId]['storage']['storageType'] != FIELD_COMPRESSION_NONE) {
+                    throw "DB2 with Embedded Strings has compressed field $fieldId";
+                }
+                delete  this.recordFormat[fieldId]['offset'];
+            }
+
+          //  $this->populateRecordOffsets();
+
+            if (!stringFields) {
+              //  this.detectEmbeddedStringFields();
+            }
+        }
+
+
+
+
+
+    }
+
+
+/*
+    populateIdMap()
+    {
+        this.idMap = [];
+
+        if (!this.hasIdBlock) {
+            this.recordFormat[this.idField]['signed'] = false; // in case it's a 32-bit int
+
+            let idOffset = !this.recordFormat[this.idField]['storage'] || this.recordFormat[this.idField]['storage']['storageType'] == FIELD_COMPRESSION_NONE;
+            if (idOffset) {
+                idOffset = this.recordFormat[this.idField]['offset'];
+            }
+
+            let sectionCount = this.sectionCount ? this.sectionCount : 1;
+
+            let recIndex = 0;
+            let recordCount = this.recordCount;
+
+            for (let z = 0; z < sectionCount; z++) {
+                if (this.sectionCount) {
+                    recordCount = this.sectionHeaders[$z]['recordCount'];
+                }
+                if (idOffset !== false) {
+                    // attempt shortcut so we don't have to parse the whole record
+
+                    if (this.sectionCount) {
+                        fseek($this->fileHandle, $this->sectionHeaders[$z]['offset'] + $idOffset);
+                    } else {
+                        fseek($this->fileHandle, $this->headerSize + $idOffset);
+                    }
+
+                    for ($x = 0; $x < $recordCount; $x++) {
+                        $id = current(unpack('V', str_pad(fread($this->fileHandle, $this->recordFormat[$this->idField]['size']), 4, "\x00", STR_PAD_RIGHT)));
+                        $this->idMap[$id] = $recIndex++;
+                        fseek($this->fileHandle, $this->recordSize - $this->recordFormat[$this->idField]['size'], SEEK_CUR); // subtract for the bytes we just read
+                    }
+                } else {
+                    for ($x = 0; $x < $recordCount; $x++) {
+                        $rec = $this->getRecordByOffset($recIndex, false);
+                        $id  = $rec[$this->idField];
+                        $this->idMap[$id] = $recIndex++;
+                    }
+                }
+            }
+        } else {
+            if ($this->sectionCount) {
+                $recIndex = 0;
+                for ($z = 0; $z < $this->sectionCount; $z++) {
+                    fseek($this->fileHandle, $this->sectionHeaders[$z]['idBlockPos']);
+                    for ($x = 0; $x < $this->recordCount; $x++) {
+                        $this->idMap[current(unpack('V', fread($this->fileHandle, 4)))] = $recIndex++;
+                    }
+                }
+            } else {
+                fseek($this->fileHandle, $this->idBlockPos);
+                if ($this->fileFormat == 'WDB2') {
+                    for ($x = $this->minId; $x <= $this->maxId; $x++) {
+                        $record = current(unpack('V', fread($this->fileHandle, 4)));
+                        if ($record) {
+                            $this->idMap[$x] = $record - 1;
+                        }
+                        fseek($this->fileHandle, 2, SEEK_CUR); // ignore embed string length in this record
+                    }
+                } else {
+                    for ($x = 0; $x < $this->recordCount; $x++) {
+                        $this->idMap[current(unpack('V', fread($this->fileHandle, 4)))] = $x;
+                    }
+                }
+            }
+        }
+
+        $sections = $this->sectionHeaders;
+        if (!$sections) {
+            $sections = [[
+                'copyBlockSize' => $this->copyBlockSize,
+                'copyBlockPos' => $this->copyBlockPos,
+            ]];
+        }
+
+        foreach ($sections as &$section) {
+            if ($section['copyBlockSize']) {
+                fseek($this->fileHandle, $section['copyBlockPos']);
+                $entryCount = floor($section['copyBlockSize'] / 8);
+                for ($x = 0; $x < $entryCount; $x++) {
+                    list($newId, $existingId) = array_values(unpack('V*', fread($this->fileHandle, 8)));
+                    if (!isset($this->idMap[$existingId])) {
+                        throw new \Exception("Copy block referenced ID $existingId which does not exist");
+                    }
+                    $this->idMap[$newId] = $this->idMap[$existingId];
+                }
+                ksort($this->idMap, SORT_NUMERIC);
+            }
+        }
+        unset($section);
 
 
 
@@ -275,8 +520,23 @@ class Reader {
 
 
     }
+*/
 
 
+    get_record_value(start,size,storageType)
+    {
+       let lenght = size;
+       let part;
+       this.file_pointer+=lenght; 
+
+       part = this.file_buffer.slice(start,start+size);
+
+       if(storageType==5)
+       {
+         return part.readUIntLE(0,size);  
+       }
+
+    }
 
 
     get_int_value(start,type = 'uint32') // default uint32
@@ -289,7 +549,7 @@ class Reader {
            lenght = 4; 
            this.file_pointer+=lenght; 
            part = this.file_buffer.slice(start,start+lenght);
-           console.log(this.file_buffer.slice(start,start+lenght).readInt32LE());
+           //console.log(this.file_buffer.slice(start,start+lenght).readInt32LE());
            return this.file_buffer.slice(start,start+lenght).readInt32LE(); 
         }
 
@@ -298,10 +558,9 @@ class Reader {
            lenght = 2; 
            this.file_pointer+=lenght; 
            part = this.file_buffer.slice(start,start+lenght);
-           console.log(this.file_buffer.slice(start,start+lenght).readInt16LE());
+          // console.log(this.file_buffer.slice(start,start+lenght).readInt16LE());
            return this.file_buffer.slice(start,start+lenght).readInt16LE(); 
         }
-
 
     }
 
